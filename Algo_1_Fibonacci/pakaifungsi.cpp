5d07de001a7cf802dd33f717edb4e0dcd9ab8259
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-03-12 10:26:12 +0800

    fibonacci pakai fungsi

--- Algo_1_Fibonacci/pakaifungsi.cpp
+++ Algo_1_Fibonacci/pakaifungsi.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void fibonacci(int prev2, int prev1, &count) {
+void fibonacci(int prev2, int prev1, int &count) {
     int newFibo;
     if (count < 10) {
         newFibo = prev2 + prev1;
+        cout << newFibo << " ";
+        prev2 = prev1;
+        prev1 = newFibo;
+        count += 1;
+        fibonacci(prev2, prev1, count);
     }
 }
 int main() {
     int count = 2;
     cout << "0" << " ";
     cout << "1" << " ";
-    fibonacci(0, 1);
+    fibonacci(0, 1, count);
 }
